0a9b7043ad2caa10edc7275d712ae195d97ba46d
Asanga Chan <cspl.itm@googlemail.com>
2022-06-10 00:46:13 +0530

    Add numaric Ententions Fine tune Controller logic

--- FunctionApi/Cushwake.Treasury.FunctionApi/Controllers/TestController.cs
+++ FunctionApi/Cushwake.Treasury.FunctionApi/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading.Tasks;
 using AzureFunctions.Extensions.Swashbuckle.Attribute;
 using Cushwake.Treasury.Application.TodoItems.Queries.GetTodoItemsWithPagination;
+using Cushwake.Treasury.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -24,31 +25,43 @@ internal class TestController : ApiController
     [ProducesResponseType((int)HttpStatusCode.BadRequest)]
     [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
     [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
-    [FunctionName("GetAtachment")]
-    [QueryStringParameter("BudgetId", "", DataType = typeof(string), Required = false)]
-    [QueryStringParameter("PageNumber", "", DataType = typeof(string), Required = false)]
-    [QueryStringParameter("PageSize", "", DataType = typeof(string), Required = false)]
+    [FunctionName("GetTodoItemsWithPagination")]
+    [QueryStringParameter("ListId", "", DataType = typeof(int), Required = false)]
+    [QueryStringParameter("PageNumber", "", DataType = typeof(int), Required = false)]
+    [QueryStringParameter("PageSize", "", DataType = typeof(int), Required = false)]
     [FixedDelayRetry(2, "00:00:10")]
-    public async Task<IActionResult> GetAtachmentService(
-     [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "attachment")]
+    public async Task<IActionResult> GetTodoItemsWithPagination(
+     [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "GetTodoItemsWithPagination")]
           HttpRequest request,
      ILogger logger)
     {
         try
         {
+            //Log the calling status to log
             logger.LogInformation("GetAtachmentServices called");
             var type = "API Call";
+
+            //Accuaring the URL parameters
+            string listId = request.Query["ListId"];
+            string pageNumber = request.Query["PageNumber"];
+            string pageSize = request.Query["PageSize"];
+
+            //Create the CQRS Query to send
             var query = new GetTodoItemsWithPaginationQuery()
             {
-                ListId = 1,
-                PageNumber = 1,
-                PageSize = 10,
+                ListId = listId.ToInteger(),
+                PageNumber = pageNumber.ToInteger(),
+                PageSize = pageSize.ToInteger(),
             };
+
+            //Inject the Query to Mediator to send 
             var result = await Mediator.Send(query);
+            //Handling output values and send IActionResult based on what we have
             return result != null ? SendSuccessfulGetResult(type, result) : SendNotFoundResult($"Table '{type}' not found", type, logger);
         }
         catch (Exception ex)
         {
+            //in case of an error we will send Error decorated IActionResult result
             return SendFailedResult(ex, "API", logger);
         }
 
---
+++ Src/Cushwake.Treasury.Infrastructure/Extensions/NumaricExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cushwake.Treasury.Infrastructure.Extensions
+{
+    public static class NumaricExtentions
+    {
+        public static int ToInteger(this string source) { 
+            int holder = 0;
+            int.TryParse(source,out holder);
+            return holder;
+        }
+    }
+}
